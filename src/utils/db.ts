b686328ebe8addcf381439707f6bb4e012fb1e9e
Gwen <gwendoline.garlet@live.fr>
2024-11-21 15:04:33 +0100

    feat: Ajout du fichier .env et des variables d'environnement

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,13 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 // Configuration de connection avec la BDD
 const db = new pg.Pool({
-  user: "user",
-  password: "pass",
-  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: 5432,
-  database: "beer-db",
+  database: process.env.DB_NAME,
 });
 
 export default db;
