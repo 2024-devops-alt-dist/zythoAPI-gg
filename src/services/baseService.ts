f62a7c70bb175adf7d4b6c8ee02d96af78b268b9
Gwen <gwendoline.garlet@live.fr>
2024-12-17 15:39:57 +0100

    Création du BaseService

---
+++ src/services/baseService.ts
@@ -0,0 +1,120 @@
+import { QueryResult } from "pg";
+import { BrewerieInterface } from "../models/brewerie";
+import db from "../utils/db";
+import { BeerInterface } from "../models/beer";
+
+export class BaseService {
+  constructor(private tableName: string) {
+    this.tableName = tableName;
+  }
+
+  /**
+   * Cette fonction récupère toutes les ressources depuis la BDD et renvoie une promise
+   *
+   * @returns Un promise de type BeerInterface ou BrewerieInterface
+   */
+  getAll = async (): Promise<BeerInterface[] | BrewerieInterface[]> => {
+    try {
+      const query = `SELECT * FROM ${this.tableName}`;
+      const { rows } = await db.query(query);
+      return rows;
+    } catch (error) {
+      throw new Error(
+        `Erreur lors de la récupération des données de ${this.tableName}`
+      );
+    }
+  };
+
+  /**
+   * Cette fonction récupère une ressource spécifique en fonction de son ID
+   *
+   * @param id l'id de la ressource à récupérer
+   * @param nameId Le nom de l'id
+   * @returns Promise de type BeerInterface, BrewerieInterface ou null
+   */
+  findById = async (
+    id: string,
+    nameId: string
+  ): Promise<BeerInterface | BrewerieInterface | null> => {
+    try {
+      const query = `SELECT * FROM ${this.tableName} WHERE ${nameId} = $1`;
+      const { rows } = await db.query(query, [id]);
+      if (rows.length === 0) {
+        return null; // Retourne null si aucune donnée trouvée
+      }
+      return rows[0];
+    } catch (error) {
+      throw new Error(
+        `Erreur lors de la récupération des données de ${this.tableName}`
+      );
+    }
+  };
+
+  /**
+   * Cette fonction permet de supprimmer la ressource avec son id
+   *
+   * @param id L'id de la ressource à supprimer
+   * @param nameId Le nom de l'id
+   */
+  deleteById = async (id: string, nameId: string): Promise<void> => {
+    try {
+      const query = `DELETE FROM ${this.tableName} WHERE ${nameId} = $1`;
+      await db.query(query, [Number(id)]);
+    } catch (error) {
+      throw new Error(
+        `Erreur lors de la suppression des données de ${this.tableName}`
+      );
+    }
+  };
+
+  /**
+   * Cette fonction permet de créer une ressource
+   *
+   * @param data Un objet de type BeerInterface ou BrewerieInterface
+   * @returns Une Promise de type BeerInterface ou BrewerieInterface
+   */
+  create = async (data: {
+    [key: string]: any;
+  }): Promise<BeerInterface | BrewerieInterface> => {
+    try {
+      const keys = Object.keys(data).join(", "); // Extrait le nom de la colonne
+      const placeholders = Object.keys(data)
+        .map((_, index) => `$${index + 1}`) // Ex: "$1, $2"
+        .join(", ");
+      const values = Object.values(data); // Extrait les valeurs de la requête
+
+      const query = `INSERT INTO ${this.tableName} (${keys}) VALUES (${placeholders}) RETURNING *`;
+
+      const { rows } = await db.query(query, values);
+      return rows[0];
+    } catch (error) {
+      throw new Error(`Erreur lors de la création dans ${this.tableName}`);
+    }
+  };
+
+  /**
+   * Cette fonction permet de mettre à jour une ressource spécifique
+   *
+   * @param id L'id de la ressource à mettre à jour
+   * @param data Un objet de type BeerInterface ou BrewerieInterface
+   * @returns
+   */
+  upDate = async (
+    id: string,
+    data: { [key: string]: any }
+  ): Promise<BeerInterface | BrewerieInterface> => {
+    try {
+      const placeholders = Object.keys(data)
+        .map((key, index) => `${key} = $${index + 1}`) // Ex: "name = $1, country = $2"
+        .join(", ");
+      //   const values = Object.values(data); // Extrait les valeurs de la requête
+      const values = [...Object.values(data), id];
+      const query = `UPDATE ${this.tableName} SET ${placeholders} WHERE id_brewerie = $${values.length} RETURNING *`;
+
+      const { rows } = await db.query(query, values);
+      return rows[0];
+    } catch (error) {
+      throw new Error(`Erreur lors de la création dans ${this.tableName}`);
+    }
+  };
+}
