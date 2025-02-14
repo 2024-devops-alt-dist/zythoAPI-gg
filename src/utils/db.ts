234683749880e31f0fc7436d74cd0a3cd0d82e0c
Gwen <gwendoline.garlet@live.fr>
2025-02-14 13:03:11 +0100

    TEST - deployement

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 // Configuration de connection avec la BDD
 const db = new pg.Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // port: 5432,
+  // database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Indispensable pour Neon (sinon erreur SSL)
+  },
 });
 
 export default db;
